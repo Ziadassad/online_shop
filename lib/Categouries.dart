e0d7aa4b8ac0f647407755daa394d0b9c5647ea6
ziad <ziad.assad.10@gmail.com>
2020-07-25 21:01:59 +0300

    chang

--- lib/Categouries.dart
+++ lib/Categouries.dart
@@ -23,6 +23,7 @@ class _CategouriesState extends State<Categouries> {
       });
   }
   Future<List<ModelItem>> getdata() async {
+    await Future.delayed(Duration(seconds: 1));
     return list;
   }
   @override
@@ -39,36 +40,43 @@ class _CategouriesState extends State<Categouries> {
         onRefresh: refresh,
         child: FutureBuilder(
           future: getdata(),
-          builder: (context , snapshot) => GridView.builder(
-            itemCount: list.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.75
-              ),
-              itemBuilder: (context , position) => GestureDetector(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        height: 180,
-                        width: 160,
-                        decoration: BoxDecoration(
-                            color: Colors.indigo,
-                            borderRadius: BorderRadius.circular(15)
-                        ),
-                         child: Image.memory(Utility.dataFromBase64String(list[position].image)),
+          builder: (context , snapshot) {
+           return GridView.builder(
+                itemCount: list.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.75
+                ),
+                itemBuilder: (context, position) =>
+                    GestureDetector(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            height: 180,
+                            width: 160,
+                            decoration: BoxDecoration(
+                                color: Colors.indigo,
+                                borderRadius: BorderRadius.circular(15)
+                            ),
+                            child: Image.memory(
+                                Utility.dataFromBase64String(list[position]
+                                    .image)),
+                          ),
+                          Text(snapshot.data[position].name,
+                            style: TextStyle(color: Colors.grey),),
+                          SizedBox(height: 5,),
+                          Text("${snapshot.data[position].price} \$")
+                        ],
                       ),
-                      Text(snapshot.data[position].name, style: TextStyle(color: Colors.grey),),
-                      SizedBox(height: 5,),
-                      Text("${snapshot.data[position].price} \$")
-                    ],
-                  ),
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => DetialItem(list[position]),));
-                  },
-              )
-          ),
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => DetialItem(list[position]),));
+                      },
+                    )
+            );
+          }
         ),
       ),
     );
